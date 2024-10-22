b4d45eb9fc14ae7d1c8fd2d42a0b2190e18ced0d
khairul-islam98 <janeyrahman900@gmail.com>
2024-10-23 02:00:11 +0600

    create enrollment table

--- solution.sql
+++ solution.sql
@@ -1,9 +1,9 @@
 -- Active: 1729099285669@@127.0.0.1@5432@university_db
 
 -- create database
-CREATE DATABASE university_db
+CREATE DATABASE university_db;
 
--- create student Table
+-- create student table
 CREATE TABLE students(
     student_id SERIAL PRIMARY KEY,
     student_name VARCHAR(100),
@@ -12,11 +12,18 @@ CREATE TABLE students(
     frontend_mark INTEGER,
     backend_mark INTEGER,
     status VARCHAR(15)
-)
+);
 
--- create a courses
+-- create a courses table
 CREATE TABLE courses(
     course_id SERIAL PRIMARY KEY,
     course_name VARCHAR(50),
     credits INTEGER
-)
+);
+
+-- create an enrollment table
+CREATE TABLE enrollment(
+    enrollment_id SERIAL PRIMARY KEY,
+    student_id INTEGER REFERENCES students(student_id),
+    course_id INTEGER REFERENCES courses(course_id)
+);
