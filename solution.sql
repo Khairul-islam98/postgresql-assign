f911d37c05094ff2ae27ee30972273df348804cc
khairul-islam98 <janeyrahman900@gmail.com>
2024-10-23 01:50:52 +0600

    create student table

---
+++ solution.sql
@@ -0,0 +1,16 @@
+-- Active: 1729099285669@@127.0.0.1@5432@university_db
+
+-- create database
+CREATE DATABASE university_db
+
+-- create student Table
+CREATE TABLE students(
+    student_id SERIAL PRIMARY KEY,
+    student_name VARCHAR(100),
+    age INTEGER,
+    email VARCHAR(100),
+    frontend_mark INTEGER,
+    backend_mark INTEGER,
+    status VARCHAR(15)
+)
+
