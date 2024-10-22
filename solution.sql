bbe55d87cc21ba77c3c1405b72cbfe686b281cb1
khairul-islam98 <janeyrahman900@gmail.com>
2024-10-23 02:51:23 +0600

    query 2 solve retrive names of all student enrolled in course

--- solution.sql
+++ solution.sql
@@ -29,32 +29,37 @@ CREATE TABLE enrollment(
 );
 
 -- insert data students table
-INSERT INTO students(student_id, student_name, age, email, frontend_mark, backend_mark, status)
+INSERT INTO students(student_name, age, email, frontend_mark, backend_mark, status)
 VALUES
-(1, 'Sameer', 21, 'sameer@example.com', 48, 60, NULL),
-(2, 'Zoya', 23, 'zoya@example.com', 52, 58, NULL),
-(3, 'Nabil', 22, 'nabil@example.com', 37, 46, NULL),
-(4, 'Rafi', 24, 'rafi@example.com', 41, 40, NULL),
-(5, 'Sophia', 22, 'sophia@example.com', 50, 52, NULL),
-(6, 'Hasan', 23, 'hasan@gmail.com', 43, 39, NULL);
+('Sameer', 21, 'sameer@example.com', 48, 60, NULL),
+('Zoya', 23, 'zoya@example.com', 52, 58, NULL),
+('Nabil', 22, 'nabil@example.com', 37, 46, NULL),
+('Rafi', 24, 'rafi@example.com', 41, 40, NULL),
+('Sophia', 22, 'sophia@example.com', 50, 52, NULL),
+('Hasan', 23, 'hasan@gmail.com', 43, 39, NULL);
 
 -- insert data courses table
-INSERT INTO courses(course_id, course_name, credits)
+INSERT INTO courses(course_name, credits)
 VALUES
-(1, 'Next.js', 3),
-(2, 'React.js', 4),
-(3, 'Databases', 3),
-(4, 'Prisma', 3);
+('Next.js', 3),
+('React.js', 4),
+('Databases', 3),
+('Prisma', 3);
 
 -- insert data enrollment table
-INSERT INTO enrollment(enrollment_id, student_id, course_id)
+INSERT INTO enrollment(student_id, course_id)
 VALUES
-(1, 1, 1),
-(2, 1, 2),
-(3, 2, 1),
-(4, 3, 2);
+(1, 1),
+(1, 2),
+(2, 1),
+(3, 2);
 
 -- Query 1: Insert a new student data
 INSERT INTO students(student_name, age, email, frontend_mark, backend_mark, status)
 VALUES ('Khairul Islam', 25, 'janeyrahman900@gmail.com', 59, 60, NULL);
 
+-- Query 2: Retrieve the names of all students who are enrolled in the course
+SELECT student_name FROM students
+JOIN enrollment ON students.student_id = enrollment.student_id
+JOIN courses ON courses.course_id = enrollment.course_id
+WHERE course_name = 'Next.js';
